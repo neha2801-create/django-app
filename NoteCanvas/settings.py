180fc7a73689ab1d94ed2cd118627fd80bcf8bdf
Neha <nehapathak280197@outlook.com>
2024-05-02 23:04:00 -0400

    Update settings.py

--- NoteCanvas/settings.py
+++ NoteCanvas/settings.py
@@ -83,10 +83,7 @@ WSGI_APPLICATION = 'NoteCanvas.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
