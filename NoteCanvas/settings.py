75c965b6163a359cbeb9573a5c699d3ca6d99f71
Neha <nehapathak280197@outlook.com>
2024-05-02 22:50:37 -0400

    changes

--- NoteCanvas/settings.py
+++ NoteCanvas/settings.py
@@ -131,14 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-<<<<<<< HEAD:Backend/NoteCanvas/NoteCanvas/settings.py
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-=======
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
->>>>>>> 2f9f1bd500ae5ca274489084219e0935a60b02a6:NoteCanvas/settings.py
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
